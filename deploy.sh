f6ac33550e2a0102d37ef809a07e89d6bf64480f
Sorin Lacriteanu <lacritea@adobe.com>
2020-02-21 09:51:27 +0200

    fix travis indenting

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 docker build -t sorinlacriteanu/multi-client:latest -t sorinlacriteanu/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t sorinlacriteanu/multi-server:latest -t sorinlacriteanu/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t sorinlacriteanu/multi-worker:latest -t sorinlacriteanu/multi-worker:$SHA -f ./worker/Dockerfile ./worker
@@ -12,7 +10,7 @@ docker push sorinlacriteanu/multi-client:$SHA
 docker push sorinlacriteanu/multi-server:$SHA
 docker push sorinlacriteanu/multi-worker:$SHA
 
-kubectl apply -f ./k8s
+kubectl apply -f k8s
 
 kubectl set image deployments/server-deployment:$SHA server=sorinlacriteanu/multi-server
 kubectl set image deployments/client-deployment:$SHA client=sorinlacriteanu/multi-client
